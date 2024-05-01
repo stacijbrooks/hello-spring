c983321a1517285a26949c9a66354d3b35921b74
stacijbrooks <stacibrooksfitness@gmail.com>
2024-04-30 22:54:29 -0500

    Hello Thymeleaf

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -3,86 +3,36 @@ package org.launchcode.hellospring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Created by Chris Bay
+ */
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
 
-    @RequestMapping(value="hello", method = RequestMethod.POST)
-    @ResponseBody
-    public String helloPost(@RequestParam String name, @RequestParam String language) {
-        if (name == null) {
-            name = "World";
-        }
-
-        return createMessage(name, language);
-
-        // For a bonus mission, students can change this response text to look nicer.
-        // This is subjective, but students should be modifying the HTML of the response string.
-    }
-
-    public static String createMessage(String n, String l) {
-        String greeting = "";
-
-        if (l.equals("english")) {
-            greeting = "Hello";
-        }
-        else if (l.equals("french")) {
-            greeting = "Bonjour";
-        }
-        else if (l.equals("italian")) {
-            greeting = "Bonjourno";
-        }
-        else if (l.equals("spanish")) {
-            greeting = "Hola";
-        }
-        else if (l.equals("german")) {
-            greeting = "Hallo";
-        }
-
-        return greeting + " " + n;
-    }
-
-//    //Handles request at /hello
-//    @GetMapping("hello") //will accept git requests
-//    @ResponseBody //return plain text response, until we learn templates
-//    public String hello() {
-//        return "Hello, Spring!";
-//    }
-
     @GetMapping("goodbye")
-    //@ResponseBody
+    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
-    //live /hello/hello
-    //Handles request of the form /hello?name=LaunchCode
+    // Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-   // @ResponseBody
+    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
-    //Handles requests of the form /hello/LaunchCode
+    // Handles requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
-   // @ResponseBody
+    @ResponseBody
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
-    // lives at  /hello/form
+    // /hello/form
     @GetMapping("form")
-    //@ResponseBody
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" +
-                "<input type='text' name= 'name'>" +
-                "<input type='submit' value='Greet me!'" +
-                "<form>" +
-                "<body>" +
-                "<html>";
+        return "form";
     }
-}
 
+}
